7c738212ab29967da8ba6aac6b27585fd933cd25
Paloma17ga <45967301+Paloma17ga@users.noreply.github.com>
2019-01-26 11:14:17 +0100

    Add files via upload

---
+++ crearusuarios.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+################
+#Autor:
+#Descripcion: Script que pide nombre de usuario y otros datos 
+#y permite crear usuarios hasta que se escriba salir
+#################
+
+clear
+echo "Introduzca nombre para nuevo usuario - salir para cerrar el script"
+read usuario
+
+while [ $usuario != "salir" ]; do
+	useradd -m -d /home/$home $usuario
+#	Añade contraseña a usuario
+#	passwd $usuario
+	clear
+	echo "-- Usuario $usuario añadido correctamente --"
+	echo "Introduzca otro nombre para nuevo usuario - salir para cerrar el script"
+	read usuario
+done
