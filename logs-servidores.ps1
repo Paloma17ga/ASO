b6154b356f99f6f561e8807fc9e81d6c01ba60a6
Paloma17ga <45967301+Paloma17ga@users.noreply.github.com>
2019-02-12 17:33:56 +0100

    Update logs-servidores.ps1

--- logs-servidores.ps1
+++ logs-servidores.ps1
@@ -12,25 +12,25 @@ Import-Csv .\ip_servidores.csv|foreach{
     Get-Eventlog -Computer $ip -List |select log | ConvertTo-Csv > log_sevidores.csv
 
     Import-Csv .\log_servidores.csv|foreach{
-        $log=$_.log
+        $nombrelog=$_.log
         cls
         Write-Host "Estos son todos los logs registrados del sistema"
         Get-EventLog -List
-        $confir=Read-Host "Desea visualizar los eventos de $log s/n"
+        $confir=Read-Host "Desea visualizar los eventos de $nombrelog s/n"
     
         if ($confir -eq 's')
             {
             $opcion = Read-Host "Introduzca un tipo de entrada (Error,Information,FailureAudit,SuccessAudit,Warning) "
-            $web=Read-Host "Desea generar un documento html del log $log y tipo de entrada $opcion s/n"
+            $web=Read-Host "Desea generar un documento html del log $nombrelog y tipo de entrada $opcion s/n"
             if ($web -eq "s"){
-                Get-EventLog -Computer $ip -Logname $log -EntryType $opcion
-                Get-Eventlog -Computer $ip -LogName $log -EntryType $opcion |select eventid,message |convertTo-html -Title "Log $nombrelog Tipo $opcion" -Head '<meta http-equiv="refresh" content="5">' -pre "<h1>LOG $nombrelog TIPO $opcion</h1>" > $pathLogs$nombrelog.html;ii $pathLogs$nombrelog.html
+                Get-EventLog -Computer $ip -Logname $nombrelog -EntryType $opcion
+                Get-Eventlog -Computer $ip -LogName $nombrelog -EntryType $opcion |select eventid,message |convertTo-html -Title "Log $nombrelog Tipo $opcion" -Head '<meta http-equiv="refresh" content="5">' -pre "<h1>LOG $nombrelog TIPO $opcion</h1>" > $pathLogs$nombrelog.html;ii $pathLogs$nombrelog.html
                 }
             else {Read-Host "Pulse una tecla"}        
             }
-            $borrar = Read-Host "Desea eliminar logs de $log s/n"
+            $borrar = Read-Host "Desea eliminar logs de $nombrelog s/n"
             if ($borrar -eq "s"){
-                Clear-EventLog -LogName $log
+                Clear-EventLog -LogName $nombrelog
             else {Read-Host "Pulse una tecla"}
             }
         else {Read-Host "Pulse una tecla"}
